ad3decfbefc3981159a147cc2971912ec7776c5d
Snehalikalall <snehaisha123@gmail.com>
2021-06-26 07:31:15 +0200

    Add files via upload

---
+++ R/DataProcessing.R
@@ -0,0 +1,30 @@
+normalized_data <- function (raw_Data, min_Reads = 5, min_Cell = 0.1, min_Gene = 1000,
+                             log = T)
+{
+ # library(SingleCellExperiment)
+#  library(edgeR)
+  #library(scDatasets)
+  #library(biomaRt)
+#  library('Linnorm')
+
+  #cell Filtering
+  MIN = min(raw_Data)
+  good_Cells = apply(raw_Data, 2, function(x) sum(x > MIN)) >= min_Gene
+  temp_Data = raw_Data[, good_Cells]
+
+  #Gene filtering
+  C = floor(dim(temp_Data)[2] * min_Cell)
+  exprs_Genes = apply(temp_Data, 1, function(x) sum(x > min_Reads)) >= C
+  temp_Data = temp_Data[exprs_Genes, ]
+
+  cat(paste("Remaining genes:", dim(temp_Data)[1], "\n", sep = ""))
+  cat(paste("Remaining cells:", dim(temp_Data)[2], sep = ""))
+  norm_Data = Linnorm.Norm(temp_Data)
+  if (log == T) {
+    norm_log_Data = log2(1 + norm_Data)
+  }
+  else {
+    norm_log_Data = norm_Data
+  }
+  return(norm_log_Data)
+}
---
+++ R/Feat_Renyi.R
@@ -0,0 +1,81 @@
+Renyifeaturedata <- function(data,cell,p,q,nf){
+ # library('MASS')
+#  library(foreach)
+#  library(doParallel)
+#  library('Rfast')
+
+  jointprob_2 <-function(u) {
+    ncol=length(unique(u[,1]))
+    nrow=length(unique(u[,2]))
+    probtable=matrix(0,nrow,ncol)
+    a=unique(u[,1])
+    for(i in 1:length(a))
+    {
+      index=which(u[,1]==a[i])
+      pmat1=as.matrix(u[index,2])
+      s1=aggregate(data.frame(count = pmat1), list(value = pmat1), length)
+      lens1=length(s1$count)
+      probtable[1:lens1,i]=(s1$count)/nrow(data)
+    }
+    probtable
+  }
+  jointprob_3 <-function(u) {
+    ncol=length(unique(u[,1]))
+    nrow=length(unique(u[,2]))*length(unique(u[,3]))
+    probtable=matrix(0,nrow,ncol)
+    a=unique(u[,1])
+    for(i in 1:length(a))
+    {
+      index=which(u[,1]==a[i])
+      pmat=as.matrix(u[index,2:3])
+      b=unique(pmat[,1])
+      count=1
+      for(j in 1: length(b))
+      {
+        index1=which(pmat[,1]==b[j])
+        pmat1=pmat[index1,2]
+        s1=aggregate(data.frame(count = pmat1), list(value = pmat1), length)
+        lens1=length(s1$count)
+        probtable[(count:(count+lens1-1)),i]=(s1$count)/nrow(data)
+        count=count+lens1
+      }
+    }
+    probtable
+  }
+
+
+  cl <- makeCluster(p)
+  registerDoParallel(cl)
+  classc=cell
+  fea<- matrix(0, nrow=1,ncol =nf)
+  parl<-foreach(j=1:count, .combine=c,.packages= c("Rfast")) %dopar%
+    {
+      u<-as.matrix(cbind(classc,data[,j]))
+      tmat=jointprob_2(u)
+      tmat=tmat^q
+      transvecfea=(q/(1-q))*(log(sum((rowsums(tmat))^(1/q))))
+    }
+
+  mimc1<-as.matrix(parl)
+  fea[1,1]=which.max(mimc1)
+  idx=which.max(mimc1)
+
+  for(i in 1:(nf-1))
+  {
+    parl<-foreach(j=1:count, .combine=c,.packages= c("Rfast")) %dopar%
+      {
+        u2<-as.matrix(cbind(classc,data[,idx],data[,j]))
+        tmat=jointprob_3(u2)
+        tmat=tmat^q
+        transvecfea=(q/(1-q))*(log(sum((rowsums(tmat))^(1/q))))
+      }
+    feat<-as.matrix(parl)
+    feat[fea[1:i]]<-Inf
+    idx=which(feat==min(feat[which(feat>0)]))[1]
+    fea[1,(i+1)]=idx
+  }
+  stopCluster(cl)
+  Renyidata=data[,fea] # Feature reduced Data with renyi entropy
+
+  return(Renyidata)
+}
